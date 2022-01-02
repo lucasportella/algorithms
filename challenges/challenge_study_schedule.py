39993de0fe4a298f84d4b05ab1fa9942bac9cd35
lucas <lucasportellaagu@gmail.com>
2022-01-02 12:38:32 -0300

    req 1 fixed - mixed or by and

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,8 @@
-def input_check(input):
-    if type(input[0]) == int and type(input[1]) == int:
-        return True
-    return False
-
-def study_schedule(permanence_period, target_time='missing'):
-    if target_time == 'missing' or type(target_time) != int: return None
+def study_schedule(permanence_period, target_time):
+    if type(target_time) != int: return None
     counter = 0
     for c in permanence_period:
-        if not input_check(c): return None
+        if type(c[0]) != int or type(c[1]) != int: return None
         if target_time in c or (c[0] < target_time and c[1] > target_time):
             counter += 1
     return counter
