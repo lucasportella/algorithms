e95a86aa3c7b90ec65f12b5ad440277a4a6a3a71
lucas <lucasportellaagu@gmail.com>
2022-01-02 14:02:27 -0300

    req 2 done

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if high_index < 0: return False
+    reversed_word = reverse(word, low_index, high_index)
+    if reversed_word == word:
+        return True
+    return False
+
+
+def reverse(word, low_index, high_index):
+    if high_index == low_index:
+        return word[low_index]
+    return word[high_index] + reverse(word, low_index, high_index - 1)
