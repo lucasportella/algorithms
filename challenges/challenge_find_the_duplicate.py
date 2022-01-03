08d35853a1d0eee2ab5c0ebf4e11e051447b3011
lucas <lucasportellaagu@gmail.com>
2022-01-03 10:44:42 -0300

    req 4 done

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,10 @@
+# não entendi como deu complexidade O(n log n), visto que o segundo loop pode atingir complexidade O(n)
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) < 2: return False
+    nums.sort()
+    for i in range(len(nums) -1):
+        if type(nums[i]) != int or nums[i] < 0: return False
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+    return False
